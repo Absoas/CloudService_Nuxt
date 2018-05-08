d9948ac9ac73909595b059733054dc97bdd39d08
YbCloud <abso8787@naver.com>
2018-05-08 12:55:49 +0900

    20180508-nuxt.js 실습

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <div class = "block">
+      <a href = "about" class = "button is-primary">Test</a>
+      <p>클라우드 서비스용 포트폴리오</p>
+      <img src="~static/logo.gif" alt=""/><br>
+
+    </div>
+    <br>
+    <nuxt-link to ="/about"></nuxt-link>
+  </section>
+</template>
+
+<script>
+import Vue from 'vue';
+</script>
